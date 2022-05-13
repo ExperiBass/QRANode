95fd4a3f0ada179b992e8970ec70336d3459a018
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2022-05-13 14:07:20 -0400

    this aint dead yet! coming back to refactor my old code uwu

--- qranode.js
+++ qranode.js
@@ -2,64 +2,86 @@
  * Fork of https://github.com/cbumgard/node-qrand
  * Fork by: GingkathFox (Now known as ExperiBass)
  */
-
+'use strict'
+//const fetch = require('fetch')
 const Axios = require('axios')
-const BASE = 'http://qrng.anu.edu.au/API/jsonI.php'
+const BASE_URL = 'http://qrng.anu.edu.au/API/jsonI.php'
 const LIMIT = 1024
+const VALID_TYPES = [
+    'uint8',
+    'uint16',
+    'hex16'
+]
 const {
     prettifiedName
 } = require('./package.json')
+function warning(msg) {
+    console.warn(`[${prettifiedName}:Warning] | ${msg}`)
+}
 
-module.exports = getRandomNumber
 /**
  * Get a random number from https://qrng.anu.edu.au/.
- * @param {string} type - Must be either `uint8`, `uint16`, or `hex16`. Defaults to `uint8`.
+ * @async
+ * @param {string} [type] - Must be either `uint8`, `uint16`, or `hex16`. Defaults to `uint8`.
  *
  * - `uint8` - returns numbers between 0 and 255.
  *
  * - `uint16` - returns numbers between 0 and 65535.
  *
- * - `hex16` - returns hexadecimal characters between `00` and `ff`.  
+ * - `hex16` - returns hexadecimal characters between `00` and `ff`.
+ * Each block is two bytes.  
  * For example, if you set `blockSize` to `4`, it would return hex between `00000000` and `ffffffff`.
  *
- * @param {number} amount - The amount of numbers to get. Max array size is `1024`.
- * @param {number} blockSize - The length of each hex block. Max block size is `1024`.  
+ * @param {number} [amount] - The amount of numbers to get. Max array size is `1024`. defaults to 1.
+ * @param {number} [blockSize] - The length of each hex block. Max block size is `1024`.  
  * Only used with `hex16`, if the `type` argument is different this doesn't matter.
- * @returns {number[]|string[]} - A array of numbers if `uint8` or `uint16` were chosen, else a array of hexadecimal strings.
- * @async
+ * @returns {number[]|string[]} - An array of numbers if `uint8` or `uint16` were chosen, else an array of hexadecimal strings.
  */
-async function getRandomNumber(type = 'uint8', amount = 1, blockSize = 1) {
+async function getRandomNumbers(dataType = "uint8", amount = 1, blockSize = 1) {
 
-    const VALID_TYPES = [
-        'uint8',
-        'uint16',
-        'hex16'
-    ]
+    // shift to lowercase
+    dataType = dataType.toLowerCase()
 
-    if (!VALID_TYPES.includes(type)) {
-        type = 'uint8'
-        console.log(`[${prettifiedName}:Warning] | The "type" argument must be one of these: ${VALID_TYPES.join(', ')}\nResetting type to "uint8".`)
+    // do a quick validation, requesting negative numbers from
+    // a quantum void would probably end the world
+    if (!VALID_TYPES.includes(dataType)) {
+        warning(`The "dataType" argument must be one of these: ${VALID_TYPES.join(', ')}\nResetting type to "uint8".`)
+        dataType = 'uint8'
     }
     if (amount < 1) {
-        console.log(`[${prettifiedName}:Warning] | The "amount" argument can't be less than one. Resetting amount to one.`)
+        warning(`The "amount" argument can't be less than one. Resetting amount to one.`)
         amount = 1
     }
     if (blockSize < 1) {
-        console.log(`[${prettifiedName}:Warning] | The "blockSize" argument can't be less than one. Resetting blockSize to one.`)
+        warning(`The "blockSize" argument can't be less than one. Resetting blockSize to one.`)
         blockSize = 1
     }
 
-    // set up the rest of the url and check `amount` and `blockSize` to make sure they're not over the limit
-    const args = `?type=${type}&length=${amount > LIMIT ? amount = LIMIT : amount}${type = 'hex16' ? `&size=${blockSize > LIMIT ? blockSize = LIMIT : blockSize}` : ''}`
+    // now make sure the "amount" argument isn't too large
+    if (amount > LIMIT) {
+        warning(`The "amount" argument is larger than the limit of ${LIMIT}. Resetting to ${LIMIT}.`)
+        amount = LIMIT
+    }
+    // define the args first
+    let args = `?type=${dataType}&length=${amount}`
+
+    // and if the user wants hexadecimal, make sure the blockSize is not too large either
+    if (dataType === 'hex16') {
+        if (blockSize > LIMIT) {
+            warning(`The "blockSize" argument is larger than the limit of ${LIMIT}. Resetting to ${LIMIT}.`)
+            blockSize = LIMIT
+        }
+        args += `&size=${blockSize}`
+    }
 
     // Time to get the data!
     try {
-        let response = await Axios.get(`${BASE}${args}`)
-        if (response.data.success) {
-            return response.data.data
-        }
-        throw new Error(`Malformed URL, please check your parameters.`)
+        const response = (await Axios.get(`${BASE_URL}${args}`)).data
+        // ^ extract the data from the Axios response
+        return response
     } catch (e) {
         throw e
     }
 }
+
+module.exports = getRandomNumbers
