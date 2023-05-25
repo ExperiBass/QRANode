bd6ad1e92337ec643fbd8a4fb52c30d423ddb680
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2023-05-25 15:22:03 -0400

    update deps, tidy up code

--- qranode.js
+++ qranode.js
@@ -25,27 +25,25 @@ function warning(msg) {
 }
 
 /**
- * Get a random number from https://quantumnumbers.anu.edu.au.
+ * Get some random numbers from https://quantumnumbers.anu.edu.au.
  * @async
  * @param {Object} args
- * @param {String} args.apiKey - Your API key.
- * @param {String} [args.dataType] - Must be either `uint8`, `uint16`, or `hex16`. Defaults to `uint8`.
- *
- * - `uint8` - returns numbers between 0 and 255.
- *
- * - `uint16` - returns numbers between 0 and 65535.
- *
- * - `hex8` - returns hexadecimal chunks between `00` and `ff`.
+ * @param {String} args.apiKey Your API key. An error will be thrown if not provided.
+ * @param {String} args.userAgent A custom user agent. If undefined, defaults to using the package name and version.
+ * @param {String} [args.dataType] Must be either `uint8`, `uint16`, or `hex16`. Defaults to `uint8`.
  * 
- * - `hex16` - returns hexadecimal chunks between `0000` and `ffff`.
- * 
- * For the hexadecimal types, each block is made up of `blockSize` chunks.
+ * - `uint8` - returns numbers between 0 and 255.  
+ * - `uint16` - returns numbers between 0 and 65535.  
+ * - `hex8` - returns hexadecimal chunks between `00` and `ff`.  
+ * - `hex16` - returns hexadecimal chunks between `0000` and `ffff`.  
+ * For the hexadecimal types, each block is made up of `args.blockSize` chunks.
  *
- * @param {Number} [args.amount] - The amount of numbers to get. Max array size is `1024`. Defaults to `1`.
- * @param {Number} [args.blockSize] - The length of each hex block. Max block size is `10`. Also defaults to `1`. 
- * Only used with `hex8` and `hex16`, if the `type` argument is different this doesn't matter.
+ * @param {Number} [args.amount] The amount of numbers to get. Max array size is `1024`. Defaults to `1`.
+ * @param {Number} [args.blockSize] The length of each hex block. Max block size is `10`. Defaults to `1`. 
+ * Only used with the hex types.
  * @returns {Object} A JSON object with the success status, the type requested, the length of the array, and the array of numbers.
- * @example // The example below is the result of a request for two hex16 numbers with a block size of 4.
+ * @example
+ * // The example below is the result of a request for two hex16 numbers with a block size of 4.
     {
     success: true,
     type: 'hex16',
@@ -53,60 +51,63 @@ function warning(msg) {
     data: [ '2f2497d207a39d67', 'dd537fa2b1c4c6b2' ]
     }
  */
-async function getRandomNumbers({apiKey, dataType = "uint8", amount = 1, blockSize = 1}) {
+async function getRandomNumbers({
+    dataType = 'uint8', amount = 1, blockSize = 1,
+    apiKey, userAgent
+}) {
+    // prepare param object
+    let reqParams = {}
+    // set the headers
+    const HEADERS = {}
+    HEADERS['x-api-key'] = apiKey
+    HEADERS['x-user-agent'] = userAgent || `${name}-v${version}`
+
 
     // if theres no API key, don't bother doing anything else
     if (!apiKey) {
-        throw Error(`The "apiKey" argument is required.`)
+        throw new Error(`The 'apiKey' argument is required.`)
     }
-    // set the headers
-    const HEADERS = {}
-    HEADERS["x-api-key"] = apiKey
-    HEADERS["x-user-agent"] = `${name}-v${version}`
-    
-    // now, start the actual request
 
-    // shift to lowercase
-    dataType = dataType.toLowerCase()
+    // shift dataType to lowercase
+    dataType = dataType?.toLowerCase()
 
-    // do a quick validation, requesting negative numbers from
+    // do some quick validation, requesting negative numbers from
     // a quantum void would probably end the world
-    if (!VALID_TYPES.includes(dataType)) {
-        warning(`The "dataType" argument must be one of these: ${VALID_TYPES.join(', ')}\nResetting type to "uint8".`)
-        dataType = 'uint8'
+    if (!dataType || !VALID_TYPES.includes(dataType)) {
+        throw new Error(`The 'dataType' argument must be one of these: ${VALID_TYPES.join(', ')}`)
     }
     if (amount < 1) {
-        warning(`The "amount" argument can't be less than one. Resetting amount to one.`)
+        warning(`The 'amount' argument can't be less than one. Resetting amount to one.`)
         amount = 1
     }
-    if (blockSize < 1) {
-        warning(`The "blockSize" argument can't be less than one. Resetting blockSize to one.`)
-        blockSize = 1
-    }
-
-    // now make sure the "amount" argument isn't too large
+    // now make sure the 'amount' argument isn't too large
     if (amount > LIMIT) {
-        warning(`The "amount" argument is larger than the limit of ${LIMIT}. Resetting to ${LIMIT}.`)
+        warning(`The 'amount' argument is larger than the limit of ${LIMIT}. Resetting to ${LIMIT}.`)
         amount = LIMIT
     }
-    // define the args first
-    let args = `?type=${dataType}&length=${amount}`
 
-    // and if the user wants hexadecimal, make sure the blockSize is not too large either
+    // if the user wants hexadecimal, make sure the blockSize is within bounds
     if (dataType === 'hex16' || dataType === 'hex8') {
+        if (blockSize < 1) {
+            warning(`The 'blockSize' argument can't be less than one. Resetting blockSize to one.`)
+            blockSize = 1
+        }
         if (blockSize > BLOCK_LIMIT) {
-            warning(`The "blockSize" argument is larger than the limit of ${BLOCK_LIMIT}. Resetting to ${BLOCK_LIMIT}.`)
+            warning(`The 'blockSize' argument is larger than the limit of ${BLOCK_LIMIT}. Resetting to ${BLOCK_LIMIT}.`)
             blockSize = BLOCK_LIMIT
         }
-        args += `&size=${blockSize}`
+        reqParams['size'] = blockSize
     }
+
+    // params validated, add to object
+    reqParams['type'] = dataType
+    reqParams['length'] = amount
 
     // Time to get the data!
     try {
-        const response = (await Axios.get(`${BASE_URL}${args}`, {
+        const response = (await Axios.get(`${BASE_URL}?${new URLSearchParams(reqParams)}`, {
             headers: HEADERS
         })).data
-        // ^ extract the data from the Axios response
         return response
     } catch (e) {
         throw e
